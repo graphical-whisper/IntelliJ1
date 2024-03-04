2ce494574435a931d9103b2e2021842c93a944a9
graphical-whisper <raulsantihallado@gmail.com>
2024-03-04 10:30:00 -0500

    starting the experience

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
+        System.out.println("Calculator App");
+        System.out.print("Insert the value");
+        Scanner consola= new Scanner(System.in);
+        var operator1 = Integer.parseInt(consola.nextLine());
+        var operator2 = Integer.parseInt(consola.next());
+        var answer= operator2+operator1;
     }
 }
